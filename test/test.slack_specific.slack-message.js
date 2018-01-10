689577a3a39dea9e2637104417fd71f7e86da532
David Bulnes <bulnes.david@gmail.com>
2018-01-10 14:29:10 -0800

    pushing slack messages test

--- test/test.slack_specific.slack-message.js
+++ test/test.slack_specific.slack-message.js
@@ -1,15 +1,29 @@
-const assert = require('assert')
-const sutil = require('../src/utils/slack')
-const message = require('../src/slack_specific/slack-mesage')
+const assert = require('assert');
+const sutil = require('../src/utils/slack');
+const message = require('../src/slack_specific/slack-mesage');
 
 describe('slack-message', () => {
   describe('uniqueUserID', () => {
     it('should combine the user_id and team_id to create a unique ID', () => {
-      let teamID = "team_id"
-      let userID = "user_id"
-      let msg = new message({team_id: teamID,
-                              user_id: userID})
-      assert.equal(msg.uniqueUserID, userID+"-"+teamID)
+      let teamID = "team_id";
+    	let userID = "user_id";
+    	let msg = new message({team_id: teamID,
+                              user_id: userID});
+      assert.equal(msg.uniqueUserID, userID+"-"+teamID);
+    })
+  })
+
+  describe('sendDMToSlackUser', () => {
+    it('should send a DM on the StarryTest slack from Starry', () => {
+    	let userID = "U8PTZ287N";
+    	let msg = new message({user_id: userID});
+  		return msg.sendDMToSlackUser(msg.user_id, msg.formatSlackAttachment("Test tip!", "good", "Testing sendDMToSlackUser"));
+    })
+
+    it('should fail to send a DM to an invalid userID on the StarryTest slack', () => {
+    	let userID = "U1234567N";
+    	let msg = new message({user_id: userID});
+  		return msg.sendDMToSlackUser(msg.user_id, msg.formatSlackAttachment("Test tip!", "good", "Testing sendDMToSlackUser"));
     })
   })
 })
--- test/test.utils.slack.js
+++ test/test.utils.slack.js
@@ -5,14 +5,14 @@ describe('slack-utils', () => {
   describe('extract user id', () => {
     it('should remove the preceeding "@" and "<" symbols and also remove anything after and including the "|" symbol', () => {
       let unescaped = "<@U12345678|dlohnes>"
-      let extracted = sutil.extractUserId(unescaped)
+      let extracted = sutil.extractUserIdFromCommand(unescaped)
       let expectedExtraction = "U12345678"
       assert.equal(expectedExtraction, extracted)
     })
 
     it("should just return the original input if it's unescaped", () => {
       let unescaped = "U12345678"
-      let extracted = sutil.extractUserId(unescaped)
+      let extracted = sutil.extractUserIdFromCommand(unescaped)
       let expectedExtraction = unescaped
       assert.equal(expectedExtraction, extracted)
     })
