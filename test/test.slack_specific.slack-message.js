b977a5dd924362fae185e0c9ec1d7cb8240bf689
Dexter Lohnes <dexterlohnes@gmail.com>
2018-01-12 22:15:15 -0800

    +Deleted bad tests and commented out server tests

--- test/test.slack_specific.slack-message.js
+++ test/test.slack_specific.slack-message.js
@@ -14,18 +14,4 @@ describe('slack-message', async () => {
       assert.equal(msg.uniqueUserID, `${teamID}.${userID}`);
     })
   })
-
-  describe('sendDMToSlackUser', () => {
-    it('should send a DM on the StarryTest slack from Starry', async () => {
-    	let userID = "U8PTZ287N";
-    	let msg = new message({user_id: userID});
-  		let result = await msg.sendDMToSlackUser(msg.user_id, msg.formatSlackAttachment("Test tip!", "good", "Testing sendDMToSlackUser"));
-    })
-
-    it('should fail to send a DM to an invalid userID on the StarryTest slack', () => {
-    	let userID = "U1234567N";
-    	let msg = new message({user_id: userID});
-  		return msg.sendDMToSlackUser(msg.user_id, msg.formatSlackAttachment("Test tip!", "good", "Testing sendDMToSlackUser"));
-    })
-  })
 })
