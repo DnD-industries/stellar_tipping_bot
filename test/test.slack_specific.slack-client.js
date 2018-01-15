b92da0f743f0f17165879abcf3992042030f955b
David Bulnes <bulnes.david@gmail.com>
2018-01-15 13:31:36 -0800

    separated slack message and slack client tests

---
+++ test/test.slack_specific.slack-client.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const SlackClient = require('../src/adapters/slack/slack-client')
+const testTimeout = 5000; //milliseconds
+
+describe('slack-message', async () => {
+ 	require('dotenv').config({path: './.env.' + process.env.NODE_ENV });
+
+  describe('sendDMToSlackUser', () => {
+    const oauth_token = process.env.SLACK_BOT_OAUTH_TOKEN;
+    let client = new SlackClient(oauth_token);
+
+    it('should send an attachment DM on the StarryTest slack from Starry', () => {
+    	let userID = "U8PTZ287N";
+      return client.sendDMToSlackUserWithAttachments(userID, client.formatSlackAttachment("Test tip!", "good", "Testing sendDMToSlackUserWithAttachments"))
+      .then((result) => {
+        //console.log("RESULT:" + JSON.stringify(result));
+        expect(result.ok).to.equal(true);
+      }); 
+    }).timeout(testTimeout);
+
+    it('should fail to send an attachment DM to an invalid userID on the StarryTest slack', () => {
+    	let userID = "U1234567N";
+      return client.sendDMToSlackUserWithAttachments(userID, client.formatSlackAttachment("Test tip!", "good", "Testing sendDMToSlackUserWithAttachments"))
+      .then((result) => {
+        throw new Error('Promise was unexpectedly fulfilled. Result: ' + result);
+      })
+      .catch((result) => {
+        console.log("RESULT:" + JSON.stringify(result));
+      })
+    }).timeout(testTimeout);
+
+    it('should send a plain text DM on the StarryTest slack from Starry', () => {
+      let userID = "U8PTZ287N";
+      return client.sendPlainTextDMToSlackUser(userID, "Testing sendPlainTextDMToSlackUser")
+      .then((result) => {
+        //console.log("RESULT:" + JSON.stringify(result));
+        expect(result.ok).to.equal(true);
+      }); 
+    }).timeout(testTimeout);
+
+    it('should fail to send a plain text DM to an invalid userID on the StarryTest slack', () => {
+      let userID = "U1234567N";
+      return client.sendPlainTextDMToSlackUser(userID, "Testing sendPlainTextDMToSlackUser")
+      .then((result) => {
+        throw new Error('Promise was unexpectedly fulfilled. Result: ' + result);
+      })
+      .catch((result) => {
+        console.log("RESULT:" + JSON.stringify(result));
+      })
+    }).timeout(testTimeout);
+  })
+})
--- test/test.slack_specific.slack-message.js
+++ test/test.slack_specific.slack-message.js
@@ -1,8 +1,4 @@
 const assert = require('assert');
-const expect = require('chai').expect;
-const sutil = require('../src/adapters/slack/utils');
-const SlackClient = require('../src/adapters/slack/slack-client')
-const testTimeout = 5000; //milliseconds
 
 describe('slack-message', async () => {
  	require('dotenv').config({path: './.env.' + process.env.NODE_ENV });
@@ -16,50 +12,5 @@ describe('slack-message', async () => {
                               user_id: userID});
       assert.equal(msg.uniqueUserID, `${teamID}.${userID}`);
     })
-  })
-
-  describe('sendDMToSlackUser', () => {
-    const oauth_token = process.env.SLACK_BOT_OAUTH_TOKEN;
-    let client = new SlackClient(oauth_token);
-
-    it('should send an attachment DM on the StarryTest slack from Starry', () => {
-    	let userID = "U8PTZ287N";
-      return client.sendDMToSlackUserWithAttachments(userID, client.formatSlackAttachment("Test tip!", "good", "Testing sendDMToSlackUserWithAttachments"))
-      .then((result) => {
-        //console.log("RESULT:" + JSON.stringify(result));
-        expect(result.ok).to.equal(true);
-      }); 
-    }).timeout(testTimeout);
-
-    it('should fail to send an attachment DM to an invalid userID on the StarryTest slack', () => {
-    	let userID = "U1234567N";
-      return client.sendDMToSlackUserWithAttachments(userID, client.formatSlackAttachment("Test tip!", "good", "Testing sendDMToSlackUserWithAttachments"))
-      .then((result) => {
-        throw new Error('Promise was unexpectedly fulfilled. Result: ' + result);
-      })
-      .catch((result) => {
-        console.log("RESULT:" + JSON.stringify(result));
-      })
-    }).timeout(testTimeout);
-
-    it('should send a plain text DM on the StarryTest slack from Starry', () => {
-      let userID = "U8PTZ287N";
-      return client.sendPlainTextDMToSlackUser(userID, "Testing sendPlainTextDMToSlackUser")
-      .then((result) => {
-        //console.log("RESULT:" + JSON.stringify(result));
-        expect(result.ok).to.equal(true);
-      }); 
-    }).timeout(testTimeout);
-
-    it('should fail to send a plain text DM to an invalid userID on the StarryTest slack', () => {
-      let userID = "U1234567N";
-      return client.sendPlainTextDMToSlackUser(userID, "Testing sendPlainTextDMToSlackUser")
-      .then((result) => {
-        throw new Error('Promise was unexpectedly fulfilled. Result: ' + result);
-      })
-      .catch((result) => {
-        console.log("RESULT:" + JSON.stringify(result));
-      })
-    }).timeout(testTimeout);
   })
 })
