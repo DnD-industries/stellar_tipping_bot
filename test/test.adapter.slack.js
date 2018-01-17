3c833db7f0878e50270f0bc0eb25840663c5dd1d
Dexter Lohnes <dexterlohnes@gmail.com>
2018-01-16 23:38:17 -0800

    +Changes to tests

--- test/test.adapter.slack.js
+++ test/test.adapter.slack.js
@@ -8,6 +8,10 @@ class TestableSlack extends Slack {
   async onRegistrationBadWallet (walletAddressGiven) {
     return "badWallet"
   }
+
+  async onRegistrationReplacedOldWallet(oldWallet, newWallet) {
+    return `${oldWallet} - ${newWallet}`
+  }
 }
 
 describe('slackAdapter', async () => {
@@ -18,22 +22,22 @@ describe('slackAdapter', async () => {
 
   beforeEach(async () => {
     const config = await require('./setup')()
-    slackAdapter = new TestableSlack(config)
-    Account = config.models.account
+    slackAdapter = new TestableSlack(config);
+    Account = config.models.account;
 
     accountWithWallet = await Account.createAsync({
       adapter: 'testing',
       uniqueId: 'team.foo',
       balance: '1.0000000',
       walletAddress: 'GDTWLOWE34LFHN4Z3LCF2EGAMWK6IHVAFO65YYRX5TMTER4MHUJIWQKB'
-    })
+    });
 
     accountWithoutWallet = await Account.createAsync({
       adapter: 'testing',
       uniqueId: 'team.bar',
       balance: '1.0000000'
     })
-  })
+  });
 
   // Validate their wallet address
   // If the user is already registered, send them a message back explaining (and what their Wallet Address is)
@@ -48,42 +52,39 @@ describe('slackAdapter', async () => {
       let msg = new Message({
           command : "register",
           text : "badwalletaddress013934888318"
-        })
+        });
 
-      let returnedValue = await slackAdapter.handleRegistrationRequest(msg)
-      assert.equal(returnedValue, "badWallet")
+      let returnedValue = await slackAdapter.handleRegistrationRequest(msg);
+      assert.equal(returnedValue, "badWallet");
     })
 
-    it ('should send a message back to the user and reject if a user has already registered with that wallet', () => {
-      let msg = new Message({
-        command : "register",
-        text    : "GDTWLOWE34LFHN4Z3LCF2EGAMWK6IHVAFO65YYRX5TMTER4MHUJIWQKB",
-        team_id : "team",
-        user_id : "foo"
-      })
-
-      slackAdapter.handleRegistrationRequest(msg).catch((reason) => {
-        if(reason === Slack.REG_FAIL_WALLET_VALIDATION) {
-          done()
-        }
-      })
+    it ('should return a message to send back to the user if a user has already registered with that wallet', async () => {
+      // let msg = new Message({
+      //   command : "register",
+      //   text    : "GDTWLOWE34LFHN4Z3LCF2EGAMWK6IHVAFO65YYRX5TMTER4MHUJIWQKB",
+      //   team_id : "team",
+      //   user_id : "foo"
+      // })
+      //
+      // let returnedValue = await slackAdapter.handleRegistrationRequest(msg);
+      // assert.equal(returnedValue, "replacedOldWallet");
     })
 
     it ('should send a message back to the user and reject if someone else has already registered with that wallet', () => {
       assert.equal(true ,false)
     })
 
-    it (`should overwrite the user's current wallet info if they have a preexisting wallet, and send an appropriate message`, () => {
+    it (`should overwrite the user's current wallet info if they have a preexisting wallet, and send an appropriate message`, async () => {
       let msg = new Message({
         command : "register",
-        text : "badwalletaddress013934888318"
+        text : "GDO7HAX2PSR6UN3K7WJLUVJD64OK3QLDXX2RPNMMHI7ZTPYUJOHQ6WTN",
+        team_id : "team",
+        user_id : "foo"
       })
 
-      slackAdapter.handleRegistrationRequest(msg).catch((reason) => {
-        if(reason === Slack.REG_FAIL_WALLET_VALIDATION) {
-          done()
-        }
-      })
+      let returnedValue = await slackAdapter.handleRegistrationRequest(msg);
+      assert.equal(returnedValue, "Hi")
+      // assert.equal(returnedValue, "GDTWLOWE34LFHN4Z3LCF2EGAMWK6IHVAFO65YYRX5TMTER4MHUJIWQKB - GDO7HAX2PSR6UN3K7WJLUVJD64OK3QLDXX2RPNMMHI7ZTPYUJOHQ6WTN");
     })
 
     it ('should otherwise save the wallet info to the database for the user and send an appropriate message back to them', () => {
