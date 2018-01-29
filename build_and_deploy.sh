8cb67089bcdda23a75eb2da83179521c032158e3
David Bulnes <bulnes.david@gmail.com>
2018-01-29 00:51:03 -0800

    travis will now trigger docker builds and push them to dockerhub

---
+++ build_and_deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker-compose build
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+docker push "${DOCKER_REPO}":"${TRAVIS_BRANCH}"_"${TRAVIS_COMMIT}"
+
+#If we are in the master branch, ssh into the production server to trigger a docker pull
+#if [ "${TRAVIS_BRANCH}" == "master" ]; then
+	#ssh deploy@ipaddr 
+  	#/bin/bash ./deploy.sh
+#fi
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-git pull
-docker build -t stellar-bot .
-
-docker stop app || true && docker rm app || true
-docker run --restart=always --link=db:db -itd --name app stellar-bot:latest
