213f5ec6f1a88849defcd9fa9bed469704c84c20
David Bulnes <bulnes.david@gmail.com>
2018-01-30 00:05:24 -0800

    testing docker webhook

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -18,7 +18,7 @@ docker push "${DOCKER_REPO}":$BUILD_TAG
 
 #If we are in the master branch, ssh into the production server to trigger a docker pull
 #Also validate this build wasn't triggered by a pull request to master
-if [ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false"]; then
-	ssh "${HOST_USER}"@"${HOST_MACHINE}" './pull_and_run_image.sh ${DOCKER_REPO}:$BUILD_TAG'
-fi
+#if [ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false"]; then
+#	ssh "${HOST_USER}"@"${HOST_MACHINE}" './pull_and_run_image.sh ${DOCKER_REPO}:$BUILD_TAG'
+#fi
  
