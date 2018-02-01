27d6fb649f4c4d851df885f78d636073f650a5c6
David Bulnes <bulnes.david@gmail.com>
2018-02-01 02:36:56 -0800

    added restart policy to all containers, and dynamically reseting the repo to the specified dangling commit from eligible dockerhub tags

--- deploy_scripts/pull_and_restart.sh
+++ deploy_scripts/pull_and_restart.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #Pulls a new docker image for davidbulnes/stellar-bot:master_latest, and brings up the new container
 
-#TODO: Integrate commit sha. Assign it as an environment variable and point to that variable in docker-compose.prod.yml
-#Save previous/current commit sha so that we can run docker rmi on that image
+#TODO: Save previous/current commit sha so that we can run docker rmi on that image
 
 if echo "$TAG" | grep -q "master"; then
 	#Set our env variable for the master tag to $TAG, passed from micro-dockerhub-hook/lib/run-script.js
-	"$STELLARBOT_MASTER_TAG" = "$TAG"
+	$STELLARBOT_MASTER_TAG = $TAG
+	#Remove the "master_" prefix from the tag to get the commit sha
+	$COMMIT_SHA=$STELLARBOT_MASTER_TAG%"master_"
+	echo "$COMMIT_SHA"
 	#Change our path to the relative path where our stellar bot docker-compose.yml resides
 	cd ..
+	#Fetch and reset our code to the latest commit on master
+	git fetch upstream master && git reset --hard $COMMIT_SHA & git clean -df
 	docker-compose pull app && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d app
 	#Change our path back
 	cd -
