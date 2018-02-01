62b214c4300371aafcaa805cb169e041b4b6a54b
David Bulnes <bulnes.david@gmail.com>
2018-02-01 01:59:56 -0800

    integrated dockerhub hook handler into docker-compose

---
+++ deploy_scripts/build_and_deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Handle line endings for unix systems
+git config core.autocrlf false
+#stash any local changes due to the build
+git stash --all
+#build our new app
+docker-compose build --no-cache
+#tag the build with the branch and shortened commit sha 
+BUILD_TAG="${TRAVIS_BRANCH}"_$(git rev-parse --short HEAD)
+echo "Build tag: "$BUILD_TAG
+
+if ["${TRAVIS_PULL_REQUEST}" == "false"]; then
+	docker tag "${DOCKER_REPO}":latest "${DOCKER_REPO}":$BUILD_TAG 
+	docker tag "${DOCKER_REPO}":latest "${DOCKER_REPO}":"${TRAVIS_BRANCH}"_latest
+	docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+	#push both the branch_latest tag and the build_tag with commit sha
+	docker push "${DOCKER_REPO}":"${TRAVIS_BRANCH}"_latest
+	docker push "${DOCKER_REPO}":$BUILD_TAG
+ else
+ 	echo "BUILD FROM PULL REQUEST NOT DEPLOYED. MUST BE TRIGGERED FROM A PUSH."
+ fi 
