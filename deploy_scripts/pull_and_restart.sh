ee82ff13ae8f9bfcb7cd6dd2a6b6552e36ce8619
David Bulnes <bulnes.david@gmail.com>
2018-02-01 02:22:07 -0800

    added validation for master branch in tag pull_and_restart.sh

--- deploy_scripts/pull_and_restart.sh
+++ deploy_scripts/pull_and_restart.sh
@@ -4,10 +4,14 @@
 #TODO: Integrate commit sha. Assign it as an environment variable and point to that variable in docker-compose.prod.yml
 #Save previous/current commit sha so that we can run docker rmi on that image
 
-#Set our env variable for the master tag to $TAG, passed from micro-dockerhub-hook/lib/run-script.js
-$STELLARBOT_MASTER_TAG = $TAG
-#Change our path to the relative path where our stellar bot docker-compose.yml resides
-cd ..
-docker-compose pull app && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d app
-#Change our path back
-cd -
+if echo "$TAG" | grep -q "master"; then
+	#Set our env variable for the master tag to $TAG, passed from micro-dockerhub-hook/lib/run-script.js
+	"$STELLARBOT_MASTER_TAG" = "$TAG"
+	#Change our path to the relative path where our stellar bot docker-compose.yml resides
+	cd ..
+	docker-compose pull app && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d app
+	#Change our path back
+	cd -
+else
+	echo "NOT PULLING: master not found in tag"
+fi
