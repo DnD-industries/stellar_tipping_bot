d14821e4a09174ac80be23bfe36b3decc1a8a282
Dexter Lohnes <dexterlohnes@gmail.com>
2018-02-09 14:56:17 -0800

    +Fixed syntax error

--- src/adapters/slack/server.js
+++ src/adapters/slack/server.js
@@ -196,7 +196,7 @@ class SlackServer {
       }
     } else {
       //If this is a GET request, use the query token, otherwise look for it in the body
-      let token = req.method === "GET" ? req.query.token : req.body.token);
+      let token = req.method === "GET" ? req.query.token : req.body.token;
       // NOTE that the /slack/interactive POST calls we receive are formatted slightly differently for some reason.
       // Instead of everything being at the req.body level it's at the req.body.payload level. No idea why, just how Slack implemented it.
       if(req.body.payload) {
