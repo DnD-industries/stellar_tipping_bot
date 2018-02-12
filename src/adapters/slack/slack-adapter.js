666dfbbe21748f35bca0ecc1e30139fd516e6a6b
Dexter Lohnes <dexterlohnes@gmail.com>
2018-02-12 08:00:46 -0800

    +Added disclaimer to balance request

--- src/adapters/slack/slack-adapter.js
+++ src/adapters/slack/slack-adapter.js
@@ -469,7 +469,7 @@ class Slack extends Adapter {
         },
         {
           "title": "Your balance",
-          "value": account.balance,
+          "value": account.balance + " XLM",
           "short": true
         },
         {
@@ -481,6 +481,11 @@ class Slack extends Adapter {
           "title": "To tip users",
           "value": "Use the /tip command",
           "short": false
+        },
+        {
+          "title": "Disclaimer",
+          "value": "This bot is not affiliated with the Stellar Development Foundation in any official capacity.\nYou should keep no more funds in this bot than you can afford to lose.\nLost funds will not be replaced. Use at your own discretion.",
+          "short": false
         }
       ]
     } else {
@@ -488,6 +493,11 @@ class Slack extends Adapter {
         {
           "title": "You must register to have a balance",
           "value": "Use the /register command",
+          "short": false
+        },
+        {
+          "title": "Disclaimer",
+          "value": "This bot is not affiliated with the Stellar Development Foundation in any official capacity.\nYou should keep no more funds in this bot than you can afford to lose.\nLost funds will not be replaced. Use at your own discretion.",
           "short": false
         }
       ]
