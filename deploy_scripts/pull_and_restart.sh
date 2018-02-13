998af06ba33c3d4d8463be432a421e4219d625ce
David Bulnes <bulnes.david@gmail.com>
2018-02-12 19:05:08 -0800

    testing if we can export our env vars through an echo

--- deploy_scripts/pull_and_restart.sh
+++ deploy_scripts/pull_and_restart.sh
@@ -13,20 +13,21 @@
 set -x
 echo $PWD
 
-#Resets the (non-dockerized) source code in pwd to the $1 argument
+#Resets the (non-dockerized) source code in pwd to the $1 branch using $2 commit sha
 #Args:
 #$1: string name of branch on origin to fetch from
 #$2: commit sha to reset onto
 git_fetch_reset () {
     #Fetch and reset our code to the latest commit on $1
-    git fetch origin $1 && git reset --hard $2 & git clean -df    
+    git fetch origin $1 && git reset --hard $2 && git clean -df    
 }
 
 #Performs a docker-compose pull and up on the app
 #Args: 
 #$1: middle sub_name of second docker-compose file to merge with the top level docker-compose.yml file.
 docker-compose_pull_up () {
-    docker-compose -f docker-compose.$1.yml pull app && docker-compose -f docker-compose.$1.yml up -d app
+    docker-compose -f docker-compose.yml -f docker-compose.$1.yml pull app
+    docker-compose -f docker-compose.yml -f docker-compose.$1.yml up -d app
 }
 
 #Removes the prefix in a tag, $1, specified by $2
@@ -49,20 +50,20 @@ if !( echo $TAG | grep -q "latest" ) ; then
     if echo "$TAG" | grep -q "master"; then
         #Change our path to the relative absolute where our stellar bot docker-compose.yml resides
         cd $STELLARBOT_MASTER_PATH #Needs to be set in /etc/environment to be available outside of just terminal sessions
-        echo $PWD
         #Set our env variable for the master tag to $TAG, and remove the prefix, storing in $PREFIX_REMOVED_TAG
         remove_tag_prefix $TAG "master_"
-        export STELLARBOT_MASTER_TAG=$TAG #Needs to be set in /etc/environment to be available outside of just terminal sessions
+        STELLARBOT_MASTER_TAG=$TAG #Needs to be set in /etc/environment to be available outside of just terminal sessions
+        echo "STELLARBOT_MASTER_TAG="$STELLARBOT_MASTER_TAG
         git_fetch_reset "master" $PREFIX_REMOVED_TAG
         docker-compose_pull_up "master"     
         cp $STELLARBOT_MASTER_PATH/deploy_scripts/pull_and_restart.sh $PWD/scripts/pull_and_restart.sh
     elif echo "$TAG" | grep -q "staging"; then
         #Change our path to the relative absolute where our stellar bot docker-compose.yml resides
         cd $STELLARBOT_STAGING_PATH #Needs to be set in /etc/environment to be available outside of just terminal sessions
-        echo $PWD
         #Set our env variable for the staging tag to $TAG, and remove the prefix, storing in $PREFIX_REMOVED_TAG
         remove_tag_prefix $TAG "staging_"    
-        export STELLARBOT_STAGING_TAG=$TAG 
+        STELLARBOT_STAGING_TAG=$TAG 
+        echo "STELLARBOT_STAGING_TAG="$STELLARBOT_STAGING_TAG
         git_fetch_reset "staging" $PREFIX_REMOVED_TAG
         docker-compose_pull_up "staging"
         #copy our script
