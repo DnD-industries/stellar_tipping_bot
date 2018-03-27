94ca2426374d9c9347efd3d2ba5bb4d701241a1e
Dexter Lohnes <dexterlohnes@gmail.com>
2018-03-27 12:14:52 -0700

    +Put tip-developers command under test

--- test/test.models.account.js
+++ test/test.models.account.js
@@ -10,6 +10,7 @@ describe('models / account', async () => {
   let Stellar
   let account
   let accountWithWallet
+  let devsAccount
 
   beforeEach(async () => {
     let config = await require('./setup')()
@@ -24,16 +25,23 @@ describe('models / account', async () => {
     }
 
     account = await Account.createAsync({
-      adapter: 'resting',
+      adapter: 'testing',
       uniqueId: 'foo',
       balance: '1.0000000'
     })
 
     accountWithWallet = await Account.createAsync({
-      adapter: 'resting',
+      adapter: 'testing',
       uniqueId: 'goodwall',
       balance: '1.0000000',
       walletAddress: 'GDO7HAX2PSR6UN3K7WJLUVJD64OK3QLDXX2RPNMMHI7ZTPYUJOHQ6WTN'
+    })
+
+    devsAccount = await Account.createAsync({
+      adapter: 'testing',
+      uniqueId: 'devAccount',
+      balance: '50.0000000',
+      walletAddress: 'GB73SDMWCR3IPS5P4Z52J5MRMCDV5HUP4WRRQU5UCDW3UCN4MJEE52NL'
     })
   })
 
@@ -52,7 +60,7 @@ describe('models / account', async () => {
       })
       await account.deposit(tx)
 
-      const reloaded = await Account.getOrCreate('resting', 'foo')
+      const reloaded = await Account.getOrCreate('testing', 'foo')
       const action = await Action.oneAsync({hash: 'hash', type: 'deposit', sourceaccount_id: reloaded.id})
 
       assert.equal(reloaded.balance, '6.0000000')
@@ -88,7 +96,7 @@ describe('models / account', async () => {
           }
 
           assert.equal(assertedExc, 'DUPLICATE_DEPOSIT')
-          const reloaded = await Account.getOrCreate('resting', 'foo')
+          const reloaded = await Account.getOrCreate('testing', 'foo')
           // 1 + the initial transaction, but not three when we deposit again
           assert.equal(reloaded.balance, '2.0000000')
           done()
@@ -99,13 +107,13 @@ describe('models / account', async () => {
 
   describe('getOrCreate', () => {
     it ('should only create a new account if it does not already exist', async () => {
-      const sameAccount = await Account.getOrCreate('resting', 'foo')
+      const sameAccount = await Account.getOrCreate('testing', 'foo')
       assert.equal(account._id, sameAccount._id)
 
-      const otherAccount = await Account.getOrCreate('resting', 'bar', {
+      const otherAccount = await Account.getOrCreate('testing', 'bar', {
         balance: '5.0000000'
       })
-      assert.equal(otherAccount.adapter, 'resting')
+      assert.equal(otherAccount.adapter, 'testing')
       assert.equal(otherAccount.uniqueId, 'bar')
       assert.equal(otherAccount.balance, '5.0000000')
     })
@@ -171,14 +179,47 @@ describe('models / account', async () => {
     })
   })
 
+  describe('tipDevelopers', () => {
+    it ('should throw an error if the user cannot pay', async () => {
+      let tipAmount = (parseFloat(accountWithWallet.balance) + 0.0000001).toString()
+      try {
+        await accountWithWallet.tipDevelopers(Stellar, devsAccount.walletAddress, new Big(tipAmount), "hash12345")
+        assert(false, "Should have fallen into catch")
+      } catch (e) {
+        assert.equal(e, 'Insufficient balance. Always check with `canPay` before tipping devs!')
+      }
+    })
+
+    it ('should throw an error if a transaction with that hash already exists. Account`s balance should not change', async () => {
+      let devsStartingBalance = devsAccount.balance
+      let startingBalance = accountWithWallet.balance
+      let withdrawalAmount = accountWithWallet.balance
+      // mock
+      let MockTransaction =
+          {
+            existsAsync: () => {
+              return true;
+            }
+          }
+      try {
+        await accountWithWallet.tipDevelopers(Stellar, devsAccount.walletAddress, new Big(withdrawalAmount), "hash12345", MockTransaction)
+        assert(false, "Should have fallen into catch")
+      } catch (e) {
+        assert.equal(e, 'DUPLICATE_DEV_TIP')
+        assert.equal(accountWithWallet.balance, startingBalance)
+        assert.equal(devsAccount.balance, devsStartingBalance)
+      }
+    })
+  })
+
   describe('walletAddressForUser', () => {
     it ('should return the user`s wallet if the user with the given uniqueId for the given adapter has a wallet address set', async () => {
-      const usersWallet = await Account.walletAddressForUser('resting', 'goodwall')
+      const usersWallet = await Account.walletAddressForUser('testing', 'goodwall')
       assert.equal(usersWallet, `GDO7HAX2PSR6UN3K7WJLUVJD64OK3QLDXX2RPNMMHI7ZTPYUJOHQ6WTN`, "User should have a wallet address")
     })
 
     it ('should return null if the given user does not have a wallet address set', async () => {
-      const usersWallet = await Account.walletAddressForUser('resting', 'foo')
+      const usersWallet = await Account.walletAddressForUser('testing', 'foo')
       assert.equal(usersWallet, null, "User should not have a wallet address")
     })
   })
